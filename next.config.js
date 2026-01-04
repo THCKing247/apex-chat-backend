f7894efeda3eda7729575d6a7c5478aa0f0b1d4e
Apex Technical Solutions Group LLC <135770058+THCKing247@users.noreply.github.com>
2026-01-04 09:33:10 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Exclude the old React app and server directories from the build
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  // Ignore patterns for webpack
+  webpack: (config, { isServer }) => {
+    // Exclude client and server directories
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/client/**', '**/server/**', '**/node_modules/**'],
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
 
